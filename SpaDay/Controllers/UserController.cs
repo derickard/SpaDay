dd3a7ba727bf1fa8525b0bc91b5e1755302532c4
derickard <derickard@gmail.com>
2020-06-22 10:15:28 -0400

    ch14 studio complete

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Add()
+        {
+            ViewBag.error = false;
+            return View();
+        }
+
+        [HttpPost("/User")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.user = newUser.Username;
+            ViewBag.email = newUser.Email;
+            ViewBag.date = newUser.Date.ToLongDateString();
+            if (newUser.Password == verify)
+            {
+                ViewBag.error = false;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = true;
+                return View("Add");
+            }
+
+
+        }
+    }
+}
